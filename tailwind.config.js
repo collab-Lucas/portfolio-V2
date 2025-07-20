0816507d1ceb3032cb9c902e6ed1587fb3c8c488
collab-Lucas <lucasbonneau9@gmail.com>
2025-07-20 17:44:49 +0200

    save20/07Marche0.6

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    './src/**/*.{html,ts}',
+    './public/index.html'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  corePlugins: {
+    preflight: false,
+  },
+};
